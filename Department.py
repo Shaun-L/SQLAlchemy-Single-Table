e5bb183e343dbe7e0eaa4e25c73c38aba32ab0ea
RodrigoBoixo <32583698+RodrigoBoixo@users.noreply.github.com>
2023-09-16 22:10:18 -0700

    Update Department.py
    added uniqueness constraints

--- Department.py
+++ Department.py
@@ -8,11 +8,14 @@ class Department(Base):
 
     __tablename__ = "departments"
     departmentName: Mapped[str] = mapped_column('name', String(50), nullable=False, primary_key=True)
-    abbreviation: Mapped[str] = mapped_column('mapped_column', String(6), nullable=False)
+    abbreviation: Mapped[str] = mapped_column('abbreviation', String(6), nullable=False)
     chairName: Mapped[str] = mapped_column('chair_name', String(80), nullable=False)
     building: Mapped[str] = mapped_column('building', String(10), nullable=False)
     officeNum: Mapped[int] = mapped_column('office', Integer, nullable=False)
     description: Mapped[str] = mapped_column('description', String(80), nullable=False)
+
+    __table_args__ = (UniqueConstraint("abbreviation", name = "departments_uk_01"),UniqueConstraint("chair_name", name = "departments_uk_02"
+UniqueConstraint("building","office", name = "departmants_uk_03"), UniqueConstraint("description", name = "departments_uk_04"))
 
     def __init__(self, department_name: str, abbr: str, chair_name: str, building: str, office: int, description: str):
         self.departmentName = department_name
